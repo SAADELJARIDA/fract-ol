535204dc93c511203f299647947d8ec4fb0a41b5
SAADELJARIDA <saadeljarida@gmail.com>
2025-03-26 13:44:21 +0000

    V2

--- fractol_events.c
+++ fractol_events.c
@@ -1,7 +1,72 @@
 #include "fractal.h"
 
-void	handle_events(&fractal)
+void	close_handler(t_fractal *fractal)
 {
-	mlx_hook(fractal->mlx_win, int x_event, int x_mask, 
-		 int (*funct)(),void *param)
+	mlx_destroy_image(fractal->mlx_connection,
+					fractal->img.img_ptr);
+	mlx_destroy_window(fractal->mlx_connection,
+						fractal->mlx_window);
+	mlx_destroy_display(fractal->mlx_connection);
+	free(fractal->mlx_connection);
+	exit(EXIT_SUCCESS);
+}
+
+int	key_handler(int keysym, t_fractal *fractal)
+{
+	if (keysym == XK_Escape)
+		close_handler(fractal);
+	if (keysym == XK_Left)
+		fractal->shift_x += (0.5 * fractal->zoom);	
+	else if (keysym == XK_Right)
+		fractal->shift_x -= (0.5 * fractal->zoom);	
+	else if (keysym == XK_Up)
+		fractal->shift_y -= (0.5 * fractal->zoom);	
+	else if (keysym == XK_Down)
+		fractal->shift_y += (0.5 * fractal->zoom);	
+	else if (keysym == XK_plus)
+		fractal->iterations_defintion += 10;
+	else if (keysym == XK_minus)	
+		fractal->iterations_defintion -= 10;
+
+	fractal_render(fractal);
+	return 0;
+}
+
+int	mouse_handler(int button, int x, int y, t_fractal *fractal)
+{
+	if (button == Button5)
+	{
+		fractal->zoom *= 0.95;
+	}
+	else if (button == Button4)
+	{
+		fractal->zoom *= 1.05;
+	}
+	// refresh
+	fractal_render(fractal);
+	return 0;
+}
+
+void	events_handler(t_fractal *fractal)
+{
+	mlx_hook(fractal->mlx_window,
+			KeyPress,
+			KeyPressMask,
+			key_handler,
+			fractal);
+	mlx_hook(fractal->mlx_window,
+			ButtonPress,
+			ButtonPressMask,
+			mouse_handler,
+			fractal);
+	mlx_hook(fractal->mlx_window,
+			DestroyNotify,
+			StructureNotifyMask,
+			close_handler,
+			fractal);
+	mlx_hook(fractal->mlx_window,
+			MotionNotify,
+			PointerMotionMask,
+			julia_track,
+			fractal);
 }
